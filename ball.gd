3a51fcf6e793a62abcf8afad894903f97c8954c3
Binh <hgqbinh2002@gmail.com>
2025-03-08 16:34:51 -0600

    Add simple auto play, fix ball double collide, and ball bounce in a reflection type way

--- ball.gd
+++ ball.gd
@@ -2,15 +2,43 @@ extends Node2D
 
 var center: Vector2
 var direction: Vector2
-var speed = 50
+
+@export var speed = 100
+var circle_radius = 8
+var circle_color = Color.BLACK
+var is_recently_touch = false
+var timeout_time = 0.1
+
 
 func _draw() -> void:
-	draw_circle(Vector2.ZERO, 8, Color.BLACK)
+	draw_circle(Vector2.ZERO, circle_radius, circle_color)
+
 
 func _ready() -> void:
 	center = get_viewport_rect().size / 2
 	position = center
 	direction = Vector2.LEFT.rotated(randf_range(-PI, PI))
+	AutoPlay.auto_play_angle = direction.angle()
+
 
 func _process(delta: float) -> void:
 	position += direction * speed * delta
+
+
+func get_unit_vector(angle: float):
+	return Vector2(cos(angle), sin(angle))
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		if !is_recently_touch:
+			var normal_vector = get_unit_vector(PlayerVariables.angle)
+			direction = direction - 2 * direction.dot(normal_vector) * normal_vector
+			print("Collide")
+			AutoPlay.auto_play_angle = direction.angle()
+			is_recently_touch = true
+			$Timer.start(timeout_time)
+
+
+func _on_timer_timeout() -> void:
+	is_recently_touch = false
--- player.gd
+++ player.gd
@@ -1,36 +1,37 @@
 extends Node2D
 
 
-@export var angle: float
 var angle_speed: float = 10
 var center: Vector2
 var prev_mouse_position: Vector2
 
-
 func get_angle() -> float:
+	if AutoPlay.is_auto_play:
+		return AutoPlay.auto_play_angle
 	return (get_viewport().get_mouse_position() - center).angle() 
 
 
 func _ready() -> void:
 	prev_mouse_position = get_viewport().get_mouse_position()
 	center = get_viewport_rect().size / 2
-	angle = get_angle()
+	PlayerVariables.angle = get_angle()
 
 
 func _process(delta: float) -> void:
 	position = center
-	angle = get_angle()
-	var rotation_change = (angle - rotation) * angle_speed * delta
+	PlayerVariables.angle = get_angle()
+	var rotation_change = (PlayerVariables.angle - rotation) * angle_speed * delta
 	rotation += rotation_change
 	
 	# To fix the bug of the player jumping when angle switch from -PI to PI
-	var mouse_position = get_viewport().get_mouse_position()
-	if prev_mouse_position.x < center.x && mouse_position.x < center.x:
-		if prev_mouse_position.y < center.y && mouse_position.y >= center.y: # Mouse move from top left to bottom left
-			rotation += 2 * PI
-			rotation -= rotation_change
-		elif prev_mouse_position.y > center.y && mouse_position.y <= center.y: # Mouse move from bottom left to top left
-			rotation -= 2 * PI
-			rotation -= rotation_change
+	if !AutoPlay.is_auto_play:
+		var mouse_position = get_viewport().get_mouse_position()
+		if prev_mouse_position.x < center.x && mouse_position.x < center.x:
+			if prev_mouse_position.y < center.y && mouse_position.y >= center.y: # Mouse move from top left to bottom left
+				rotation += 2 * PI
+				rotation -= rotation_change
+			elif prev_mouse_position.y > center.y && mouse_position.y <= center.y: # Mouse move from bottom left to top left
+				rotation -= 2 * PI
+				rotation -= rotation_change
 	
-	prev_mouse_position = mouse_position
+		prev_mouse_position = mouse_position
